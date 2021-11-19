3f4de210c104d2bd8364b213b3c0502df8dc732c
aidan <aidan@example.com>
2021-11-18 21:17:01 -0500

    feat: add Vehicle_C class

---
+++ Vehicle.cpp
@@ -0,0 +1,35 @@
+#include "Vehicle.h"
+
+#include <iostream>
+
+Vehicle_C::Vehicle_C()
+{
+	manufacturer = "";
+	yearBuilt = 0;
+}
+
+void Vehicle_C::Set_Manufacturer(std::string newManufacturer)
+{
+	manufacturer = newManufacturer;
+}
+
+std::string Vehicle_C::Get_Manufacturer() const
+{
+	return manufacturer;
+}
+
+void Vehicle_C::Set_Year_Built(int newYearBuilt)
+{
+	yearBuilt = newYearBuilt;
+}
+
+int Vehicle_C::Get_Year_Built() const
+{
+	return yearBuilt;
+}
+
+void Vehicle_C::Display_Info() const
+{
+	std::cout << "\nVehicle's manufacturer is: " << manufacturer;
+	std::cout << "\nVehicle was built in: " << yearBuilt;
+}
--- Vehicle.h
+++ Vehicle.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include <string>
+
+class Vehicle_C
+{
+private:
+	std::string manufacturer;
+	int yearBuilt;
+
+public:
+	Vehicle_C ();
+
+	void Set_Manufacturer(std::string);
+	std::string Get_Manufacturer() const;
+
+	void Set_Year_Built(int);
+	int Get_Year_Built() const;
+
+	void Display_Info() const;
+};
