efd369fd4f22301aa83fc8c0df438a371ac91064
aidan <aidan@example.com>
2021-11-19 17:57:38 -0500

    feat: popualate main.cpp
    use all 3 new classes and test their functionality

---
+++ main.cpp
@@ -0,0 +1,71 @@
+// Aidan Houck
+// CIS 1202 502
+// November 18, 2021
+// A program that uses derived classes to display some information about two types of Vehicles, cars and trucks
+
+#include "Car.h"
+#include "Truck.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	// Vehicle Section
+	Vehicle_C newVehicle;
+	std::string inputManufacturer;
+	int inputYearBuilt;
+
+	std::cout << "\nEnter the vehicle manufacturer: ";
+	std::cin >> inputManufacturer;
+	newVehicle.Set_Manufacturer(inputManufacturer);
+
+	std::cout << "\nEnter the year that the vehcile was built: ";
+	std::cin >> inputYearBuilt;
+	newVehicle.Set_Year_Built(inputYearBuilt);
+
+	newVehicle.Display_Info();
+
+	
+	// Car Section
+	Car_C newCar;
+	inputManufacturer = "";
+	inputYearBuilt = 0;
+	int inputDoorCount;
+
+	std::cout << "\nEnter the car manufacturer: ";
+	std::cin >> inputManufacturer;
+	newCar.Set_Manufacturer(inputManufacturer);
+
+	std::cout << "\nEnter the year that the car was built: ";
+	std::cin >> inputYearBuilt;
+	newCar.Set_Year_Built(inputYearBuilt);
+	
+	std::cout << "\nEnter the number of doors that the car has: ";
+	std::cin >> inputDoorCount;
+	newCar.Set_Door_Count(inputDoorCount);
+
+	newCar.Display_Info();
+
+
+	// Truck Section
+	Truck_C newTruck;
+	inputManufacturer = "";
+	inputYearBuilt = 0;
+	int inputTowingCapacity;
+
+	std::cout << "\nEnter the trunk manufacturer: ";
+	std::cin >> inputManufacturer;
+	newTruck.Set_Manufacturer(inputManufacturer);
+
+	std::cout << "\nEnter the year that the truck was built: ";
+	std::cin >> inputYearBuilt;
+	newTruck.Set_Year_Built(inputYearBuilt);
+
+	std::cout << "\nEnter the truck's max towing capacity (in pounds): ";
+	std::cin >> inputTowingCapacity;
+	newTruck.Set_Tow_Capacity(inputTowingCapacity);
+
+	newTruck.Display_Info();
+
+	return 0;
+}
